3b05a7c98e8ef46168e7dd861f51ead16606e47c
NeferMR <ricaurten@uninorte.edu.co>
2025-03-27 21:38:13 -0500

    Creación del modelo Asegurados

---
+++ AseguradosAPI/Models/Asegurados.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AseguradosAPI.Models
+{
+
+    // Creación de la clase Asegurado para el modelo de datos Asegurados con los atributos requeridos
+    public class Asegurado
+    {
+        [Key] 
+        public int NumeroIdentificacion { get; set; }
+
+        [Required(ErrorMessage = "El primer nombre es obligatorio.")]
+        public string PrimerNombre { get; set; }
+
+        public string? SegundoNombre { get; set; }
+
+        [Required(ErrorMessage = "El primer apellido es obligatorio.")]
+        public string PrimerApellido { get; set; }
+
+        [Required(ErrorMessage = "El segundo apellido es obligatorio.")]
+        public string SegundoApellido { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El valor del seguro es obligatorio.")]
+        public decimal ValorSeguro { get; set; }
+
+        public string? Observaciones { get; set; }
+    }
+}
